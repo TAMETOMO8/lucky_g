02ed9f4a15dd84afab3147e0a8458f300052f41f
Ryusei <tearaito@gmail.com>
2024-05-02 23:52:46 +0900

    Add:ボタンコンポーネントを読み込み、cardを作成

--- app/javascript/react/index.tsx
+++ app/javascript/react/index.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import CheckBox from "./top_pages/components/CheckBox";
+import Button from "./top_pages/components/Button";
 
 
 export default function TopPages() {
 
   return (
-    <div className="flex flex-col text-left">
-      <CheckBox select_text="サンプル1" />
-      <CheckBox select_text="サンプル2" />
-    </div>
+      <div className="card card-compact w-96 bg-base-300 shadow-xl mx-auto">
+        <div className="flex flex-col text-center">
+          <CheckBox select_text="サンプル1" />
+          <CheckBox select_text="サンプル2" />
+        </div>
+        <div className="text-center">
+          <Button button_title="クリック" />
+        </div>
+      </div>
   );
 }
