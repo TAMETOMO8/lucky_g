72e8f95078e16f4b5c1e334e15c2ae2bb2b8d720
Ryusei <tearaito@gmail.com>
2024-05-05 22:11:41 +0900

    Fix:useStateを使用する形に変更

--- app/javascript/react/index.tsx
+++ app/javascript/react/index.tsx
@@ -1,22 +1,60 @@
-import React from "react";
-import RadioButton from "./top_pages/components/RadioButton";
-import Button from "./top_pages/components/Button";
+import * as React from 'react'
+import { useState } from "react";
+import { useReducer } from 'react';
 
 
 export default function TopPages() {
 
+  const [ radioFirst, setRadioFirst ] = useState(1);
+  const [ textSecond, setTextSecond ] = useState('嫌い');
+  const [ radioSecond, setRadioSecond ] = useState(2);
+  const [ textThird, setTextThird ] = useState('どちらとも言えない');
+  const [ radioThird, setRadioThird ] = useState(3);
+
+  const [ count, setCount ] = useState(1);
+  const [ button, setButton ] = useState('次の質問へ')
+
+  const [selected, setSelected] = useState(0);
+
+  const questionButton = document.querySelector('#question_button');
+  const radioButton = document.querySelector('.radio-button');
+
+  const changeValue = (event) => setSelected(event.target.value);
+
+  function changeText () {
+    setCount((count) => count + 1);
+  }
+
+  function textMessage(count) {
+    if (count === 1) {
+      return 'サンプル1-1';
+    } else if (count === 2) {
+      return 'サンプル1-2';
+    } else if (count === 3) {
+      return 'サンプル1-3';
+    }
+  }
+  const textFirst = textMessage(count);
+
   return (
-      <div className="card card-compact lg:w-1/3 md:w-1/2 shadow-xl mx-auto">
+      <div className="card card-compact lg:w-1/3 md:w-1/2 shadow-xl mx-auto bg-base-200">
         <div className="card-body">
-          <h5 className="card-title justify-center text-2xl font-bold text-gray-900 dark:text-white">あなたの気分で選んでみてください</h5>
-          <div className="flex flex-col text-center bg-base-200" id="sample">
-            <RadioButton select_text="サンプル1" radio_value={1} />
-            <RadioButton select_text="サンプル2" radio_value={2} />
-            <RadioButton select_text="サンプル3" radio_value={3} />
-          </div>
-          <div className="text-center">
-            <Button button_title="クリック" button_id="question_button" />
-          </div>
+          <h5 className="card-title justify-center text-2xl font-bold text-gray-900 dark:text-white">{count}つ目の質問です</h5>
+          <label>
+            <input type="radio" name="check" className="radio-button" value={radioFirst} onClick={changeValue} />
+            {textFirst}
+          </label>
+          <label>
+            <input type="radio" name="check" className="radio-button" value={radioSecond} onClick={changeValue}/>
+            {textSecond}
+          </label>
+          <label>
+            <input type="radio" name="check" className="radio-button" value={radioThird} onClick={changeValue}/>
+            {textThird}
+          </label>
+          <button onClick={changeText} className="btn btn-sm btn-active btn-accent my-3" value={selected} id="question_button">
+            クリック
+          </button>
         </div>
       </div>
   );
