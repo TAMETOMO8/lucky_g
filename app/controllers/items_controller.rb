25bd2d0896a105be3edd5b926a2a296a416b6958
Ryusei <tearaito@gmail.com>
2024-05-18 20:16:14 +0900

    Change:axios.postによるリクエストに応答できる形に変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,12 @@
-class ItemsController < ApplicationController::API
-  def search; end
+class ItemsController < ActionController::API
+  def search
+    test = params[:value].to_i
+    if test == 9
+      send = '美味しい'
+    else
+      send = 'ビミョい'
+    end
+    message = "OK"
+    render json: { message: message, test: send }
+  end
 end
