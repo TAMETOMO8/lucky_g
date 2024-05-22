ffdd251eaa41e54d355bf9be8483932fc5a181fa
Ryusei <tearaito@gmail.com>
2024-05-22 22:52:31 +0900

    Fix:シンプルな内容に修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,12 +1,6 @@
 class ItemsController < ActionController::API
   def search
-    test = params[:value].to_i
-    if test == 9
-      send = '美味しい'
-    else
-      send = 'ビミョい'
-    end
-    message = "OK"
-    render json: { message: message, test: send }
+    results = RakutenWebService::Ichiba::Item.search(keyword: 'ゴリラ フィギュア')
+    render json: { results: results }
   end
 end
