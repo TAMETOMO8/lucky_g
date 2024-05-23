554f82ef2cfb9bacbf0776a0db732c31890d2d11
Ryusei <tearaito@gmail.com>
2024-05-23 16:34:21 +0900

    Add:Reactから送られた情報で条件分岐する処理を実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,35 @@
 class ItemsController < ActionController::API
   def search
-    results = RakutenWebService::Ichiba::Item.search(keyword: 'ゴリラ フィギュア')
-    render json: { results: results }
+    keyword_value = params[:value].to_i
+    set_word(keyword_value)
+    results = RakutenWebService::Ichiba::Item.search(keyword: @keyword)
+    render json: { results: results ,fortune_text: @fortune_text}
+  end
+
+  private
+
+  def set_word(keyword_value)
+    if keyword_value == 3
+      @keyword = 'ゴリラ フィギュア'
+      @fortune_text = '今日の運勢は・・・ゴリ吉!'
+    elsif keyword_value == 4
+      @keyword = 'ゴルゴ13'
+      @fortune_text = '今日の運勢は・・・ゴル吉!'
+    elsif keyword_value == 5
+      @keyword = '悟空'
+      @fortune_text = '今日の運勢は・・・ごく吉!'
+    elsif keyword_value == 6
+      @keyword = '五条悟'
+      @fortune_text = '今日の運勢は・・・ごじょ吉!'
+    elsif keyword_value == 7
+      @keyword = 'ギドラ'
+      @fortune_text = '今日の運勢は・・・ギド吉!'
+    elsif keyword_value == 8
+      @keyword = 'ゴーイングメリー'
+      @fortune_text = '今日の運勢は・・・ゴー吉!'
+    elsif keyword_value == 9
+      @keyword = 'ゴジラ'
+      @fortune_text = '今日の運勢は・・・ゴジ吉!'
+    end
   end
 end
