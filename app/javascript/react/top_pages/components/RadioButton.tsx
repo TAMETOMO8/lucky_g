e8b4dd04e5fb65cfe920bcf50733ea298778523c
Ryusei <tearaito@gmail.com>
2024-07-10 23:18:45 +0900

    Fix:条件分岐を削除

--- app/javascript/react/top_pages/components/RadioButton.tsx
+++ app/javascript/react/top_pages/components/RadioButton.tsx
@@ -16,26 +16,18 @@ export default function RadioButton({count, radioValue, changeValue, totalValue}
       return '運動も兼ねて走る';
     } else if (count === 3 && radioValue === 1) {
       return '野菜';
-    } else if (count === 4 && radioValue === 1 && totalValue > 0) {
-      return '検索の都合上、変なアイテムも出てきますが気にしてはいけません';
-    } 
-    else if (count === 1 && radioValue === 2) {
+    } else if (count === 1 && radioValue === 2) {
       return '可愛いもの';
     } else if (count === 2 && radioValue === 2) {
       return '慌てず冷静に歩いていく';
     } else if (count === 3 && radioValue === 2) {
       return 'ケーキ';
-    } else if (count === 4 && radioValue === 2 && totalValue > 0) {
-      return '実は選んだ要素関係なくない?と言ってはいけない';
-    } 
-    else if (count === 1 && radioValue === 3) {
+    } else if (count === 1 && radioValue === 3) {
       return '黒くてかっこいいもの';
     } else if (count === 2 && radioValue === 3) {
       return '諦める';
     } else if (count === 3 && radioValue === 3) {
       return '肉';
-    } else if (count === 4 && radioValue === 3 && totalValue > 0) {
-      return '気に入らないときはもう一度押してみましょう・・・';
     }
   }
   const radioText = radioTexts(count);
